cd67714540ee652d10cf208de3bd7df110b98d7b
ElvisRD <elvisAppsdev@gmail.com>
2026-04-01 17:55:08 -0300

    navbar responsive

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, ButtonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  openNavbar: boolean = false;
+  links = [
+    { name: 'Inicio', href: '/' },
+    { name: 'Colecciones', href: '/collections' },
+    { name: 'Hombre', href: '/men' },
+    { name: 'Mujer', href: '/women' },
+  ]
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize(event: Event): void {
+    const width = window.innerWidth;
+    if (width >= 768) {
+      this.openNavbar = false;
+    }
+  }
+
 
 }
